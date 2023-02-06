9634cea7a36f1cbf633aeea7af6d279ce3d7fedc
musichead99 <musichead99@naver.com>
2023-02-06 12:10:55 +0900

    그리디 기출문제 1번 모험가 길드

---
+++ Greedy/모험가_길드.java
@@ -0,0 +1,40 @@
+package algorithm_study.Greedy;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class 모험가_길드 {
+    public static void main(String[] args) throws IOException {
+
+        /* n(모험가의 수) 파싱 */
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        /* adventurers(각 모험가의 공포도) 파싱 */
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] adventurers = new int[n];
+        for(int i = 0; i < n; i++) {
+            adventurers[i] = Integer.parseInt(st.nextToken());
+        }
+
+        /* 모험가들을 공포도 순으로 오름차순 정렬 */
+        Arrays.sort(adventurers);
+
+        /* 그룹의 수 계산 */
+        int count = 0;
+        int result = 0;
+        for(int adventurer : adventurers) {
+            count++;
+
+            if(adventurer <= count) {
+                result++;
+                count = 0;
+            }
+        }
+
+        System.out.println(result);
+    }
+}
