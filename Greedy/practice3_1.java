ddd0d8e47e299c2729349b8af4685927cb11e8d5
musichead99 <musichead99@naver.com>
2023-02-06 10:35:31 +0900

    그리디 예제 3-1 거스름돈

--- Greedy/practice3_1.java
+++ Greedy/practice3_1.java
@@ -6,24 +6,25 @@ import java.io.InputStreamReader;
 
 /* 거스름돈 문제 */
 public class practice3_1 {
+
+    public static int[] coins = {500, 100, 50, 10};
+
     public static void main(String[] args) throws IOException {
         int count = 0;
 
-        /* 사용자로부터 거슬러 줄 돈 N을 입력받는다. */
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
 
-        /* 계산대에 있는 동전들의 종류 */
-        int[] coinType = {500,100,50,10};
+        int n = Integer.parseInt(br.readLine());
 
-        /* 큰 동전부터 순서대로 개수를 세어나간다. */
-        for(int coin : coinType) {
-            count += N / coin;
-            N %= coin;
+        br.close();
+
+        for(int coin : coins) {
+            if(n >= coin) {
+                count += n / coin;
+                n = n % coin;
+            }
         }
 
-        /* 출력 */
         System.out.println(count);
-        br.close();
     }
 }
