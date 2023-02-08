1fc9dba108a1eb147b0dc24d061e64f4cc3d9d34
musichead99 <musichead99@naver.com>
2023-02-08 10:54:58 +0900

    그리디 기출문제 4번 볼링공 고르기

---
+++ Greedy/볼링공_고르기.java
@@ -0,0 +1,33 @@
+package algorithm_study.Greedy;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class 볼링공_고르기 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+
+        int[] balls = new int[n];
+
+        for(int i = 0; i < n; i++) {
+            balls[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int count = 0;
+        for(int i = 0; i < n - 1; i++) {
+            for(int j = i + 1; j < n; j++) {
+                if(balls[i] != balls[j]) count++;
+            }
+        }
+
+        System.out.println(count);
+    }
+}
