4876bcddcbbca3103ec41a01e775759d6a8a9b5b
musichead99 <musichead99@naver.com>
2023-02-09 11:33:50 +0900

    풀이 수정

--- Greedy/볼링공_고르기.java
+++ Greedy/볼링공_고르기.java
@@ -7,6 +7,7 @@ import java.util.StringTokenizer;
 
 public class 볼링공_고르기 {
     public static void main(String[] args) throws IOException {
+        /* 입력값 받기 */
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
@@ -21,6 +22,7 @@ public class 볼링공_고르기 {
             balls[i] = Integer.parseInt(st.nextToken());
         }
 
+        /* 조합 구하기 */
         int count = 0;
         for(int i = 0; i < n - 1; i++) {
             for(int j = i + 1; j < n; j++) {
