3a06eec9d8efa1c5854513551bbc8c8df14eb86f
musichead99 <musichead99@naver.com>
2023-02-10 10:05:45 +0900

    구현 예제 2 시각

--- 구현/practice4_2.java
+++ 구현/practice4_2.java
@@ -4,21 +4,24 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-/* 0시부터 N시까지 3이 하나라도 포함된 시각의 수를 계산 */
+
 public class practice4_2 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
 
-        /* 브루트 포스로 계산한다. */
+        /* 하루 24시간의 모든 시각의 경우의 수는 24*60*60 = 86400가지이므로 브루트 포스를 통해 계산해도 시간이 초과되지 않음 */
         int count = 0;
-        for(int i = 0; i < N + 1; i++) {
+        for(int i = 0; i < N; i++) {
             for(int j = 0; j < 60; j++) {
                 for(int k = 0; k < 60; k++) {
-                    if((Integer.toString(i) + Integer.toString(j) + Integer.toString(k)).contains("3")) count++;
+                    if((Integer.toString(i) + Integer.toString(j) + Integer.toString(k)).contains("3")) {
+                        count++;
+                    }
                 }
             }
         }
+
         System.out.println(count);
         br.close();
     }
