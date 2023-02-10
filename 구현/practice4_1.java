ba919bc99c58ebada8d0e71ab7967e9195c216a2
musichead99 <musichead99@naver.com>
2023-02-10 09:56:51 +0900

    구현 여제 1번 상하좌우

--- 구현/practice4_1.java
+++ 구현/practice4_1.java
@@ -6,39 +6,52 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class practice4_1 {
-    public static void main(String[] args) throws IOException {
+
+    public static class Node {
+        int x;
+        int y;
+
+        public Node(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+
+    public static int[] x = {1, -1, 0, 0};
+    public static int[] y = {0, 0, 1, -1};
+    public static void main(String[] args) throws IOException {\
+        /* 입력값 파싱 */
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        String moveType = br.readLine();
-        StringTokenizer st = new StringTokenizer(moveType);
-        int[] coordinate = {1, 1};
+        StringTokenizer st;
 
-        while(st.countTokens() != 0) {
-            int[] nextcoord = new int[2];
-            char move = st.nextToken().charAt(0);
+        int n = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
 
-            if(move == 'L') {
-                nextcoord[0] = coordinate[0];
-                nextcoord[1] = coordinate[1] - 1;
-            } else if (move == 'R') {
-                nextcoord[0] = coordinate[0];
-                nextcoord[1] = coordinate[1] + 1;
-            } else if (move == 'U') {
-                nextcoord[0] = coordinate[0] - 1;
-                nextcoord[1] = coordinate[1];
-            } else if (move == 'D') {
-                nextcoord[0] = coordinate[0] + 1;
-                nextcoord[1] = coordinate[1];
+        /* 계획서대로 좌표를 조정 */
+        Node now = new Node(1,1);
+        while(st.hasMoreTokens()) { // StringTokenizer에 남은 토큰이 없을때까지 반복
+
+            char command = st.nextToken().charAt(0);
+
+            int tmp;
+            if(command == 'L') {
+                tmp = 3;
+            } else if(command == 'R') {
+                tmp = 2;
+            } else if(command == 'U') {
+                tmp = 1;
+            } else {
+                tmp = 0;
             }
 
-            if(nextcoord[0] <= 0 || nextcoord[0] > N || nextcoord[1] <= 0 || nextcoord[1] > N) {
-                continue;
-            } else {
-                coordinate = nextcoord;
+            /* 계획서에 따라 이동한 좌표가 맵 밖으로 벗어나지 않으면 반영한다 */
+            if(now.x + x[tmp] >= 1 && now.x + x[tmp] <= n && now.y + y[tmp] >= 1 && now.y + y[tmp] <= n) {
+                now.x += x[tmp];
+                now.y += y[tmp];
             }
         }
 
-        System.out.println(coordinate[0] + " " + coordinate[1]);
         br.close();
+        System.out.println(now.x + " " + now.y);
     }
 }
