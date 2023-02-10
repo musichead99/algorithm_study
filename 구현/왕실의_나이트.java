2f864114605980fc90233e8e512cfe2ceb641939
musichead99 <musichead99@naver.com>
2023-02-10 10:25:08 +0900

    구현 실전문제 2번 왕실의 나이트

--- 구현/왕실의_나이트.java
+++ 구현/왕실의_나이트.java
@@ -5,29 +5,24 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class 왕실의_나이트 {
+
+    static int[] x = {2, 2, -2, -2, 1, -1, 1, -1};
+    static int[] y = {1, -1, 1, -1, 2, 2, -2, -2};
     public static void main(String[] args) throws IOException {
+        /* 나이트의 좌표 읽어오기 */
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String coordString = br.readLine();
+        String coordinate = br.readLine();
 
-        /* 입력받은 문자를 파싱해서 좌표값으로 변경 */
-        int[] coord = new int[2];
-        coord[0] = coordString.charAt(0) - 'a'; // 입력받은 소문자 아스키코드값에서 'a'를 빼면 좌표값을 얻을 수 있다.
-        coord[1] = Character.getNumericValue(coordString.charAt(1)) - 1; // char형의 숫자에서 int형의 값으로 변경하는 코드
-        
-        /* knight가 이동할 수 있는 방법을 steps 배열에 모아 둠 */
-        int[][] steps = {{2, 1}, {2, -1}, {-2, 1}, {-2, -1}, {1, 2}, {-1, 2}, {1, -2}, {-1, -2}};
+        /* 나이트의 좌표값 정수값으로 파싱 */
+        int knightX = coordinate.charAt(0) - 96;
+        int knightY = Character.getNumericValue(coordinate.charAt(1));
 
-        /* knight가 이동할 수 있는 경우의 수 계산 */
+        /* 나이트가 움직일 수 있는 모든 경우의 수를 검사하여 맵을 벗어나는지 확인 */
         int count = 0;
-        int[] temp = new int[2];
-        for(int[] step : steps) {
-            temp[0] = coord[0] + step[0];
-            temp[1] = coord[1] + step[1];
-
-            if(temp[0] < 0 || temp[1] < 0 || temp[0] > 7 || temp[1] > 7) {
-                continue;
+        for(int i = 0; i < 8; i++) {
+            if(knightX + x[i] >= 1 && knightX + x[i] <= 8 && knightY + y[i] >= 1 && knightY + y[i] <= 8) {
+                count++;
             }
-            count++;
         }
 
         System.out.println(count);
