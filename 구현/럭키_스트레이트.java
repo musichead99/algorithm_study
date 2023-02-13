bda936b33d19de63969b67581d49bdc417285173
musichead99 <musichead99@naver.com>
2023-02-13 10:00:14 +0900

    구현 기출문제 1번 럭키 스트레이트

--- 구현/럭키_스트레이트.java
+++ 구현/럭키_스트레이트.java
@@ -6,21 +6,29 @@ import java.io.InputStreamReader;
 
 public class 럭키_스트레이트 {
     public static void main(String[] args) throws IOException {
+        /* 점수 입력받기 */
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         String score = br.readLine();
-        String firstHalf = score.substring(0, score.length()/2);
-        String secondHalf = score.substring(score.length()/2);
 
-        int firstSum = 0;
-        int secondSum = 0;
+        /* 점수를 반으로 나누어 앞부분과 뒷부분을 변수에 대입 */
+        String first = score.substring(0, score.length()/2);
+        String second = score.substring(score.length()/2);
 
-        for(int i = 0; i < firstHalf.length(); i++) {
-            firstSum += Character.getNumericValue(firstHalf.charAt(i));
-            secondSum += Character.getNumericValue(secondHalf.charAt(i));
+        /* 앞부분과 뒷부분의 각 자릿수의 합을 계산 */
+        String LuckyStraight = "READY";
+        int sumFirst = 0;
+        int sumSecond = 0;
+        for(int i = 0; i < first.length(); i++) {
+            sumFirst += Character.getNumericValue(first.charAt(i));
+            sumSecond += Character.getNumericValue(second.charAt(i));
         }
 
-        if(firstSum == secondSum) System.out.println("LUCKY");
-        else System.out.println("READY");
+        /* 만약 럭키 스트레이트를 사용할 수 있다면(두 합이 같다면) "LUCKY"로 변경 */
+        if(sumFirst == sumSecond) {
+            LuckyStraight = "LUCKY";
+        }
+
+        System.out.println(LuckyStraight);
+        br.close();
     }
 }
