2efb04556647be5618194f60fd21f1058bac8d30
musichead99 <musichead99@naver.com>
2023-02-20 09:38:30 +0900

    DFS/BFS 기출문제 2번 연구소

--- DFSBFS/연구소.java
+++ DFSBFS/연구소.java
@@ -20,95 +20,98 @@ class Node {
 
 public class 연구소 {
 
-    static int safeAreaCount = Integer.MIN_VALUE;
-    static int[][] map;
-    static int N;
-    static int M;
-    static int[] dx = {1, 0, 0, -1};
-    static int[] dy = {0, 1, -1, 0};
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
+    static int n;
+    static int m;
+    static int safeArea = Integer.MIN_VALUE;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
 
-        map = new int[N][M];
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
 
-        for(int i = 0; i < N; i++) {
+        /* 맵 초기화 */
+        int[][] map = new int[n][m];
+        for(int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
-            for(int j = 0; j < M; j++) {
+            for(int j = 0; j < m; j++) {
                 map[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
-        dfs(0);
+        dfs(map, 0);
 
-        System.out.println(safeAreaCount);
+        System.out.println(safeArea);
     }
 
-    private static void dfs(int count) {
+    /* 맵에 3개의 벽을 세우는 dfs 함수 */
+    public static void dfs(int[][] map, int count) {
         if(count == 3) {
-            bfs();
+            bfs(map);
             return;
         }
 
-        for(int i = 0; i < N; i++) {
-            for(int j = 0; j < M; j++) {
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
                 if(map[i][j] == 0) {
                     map[i][j] = 1;
-                    dfs(count + 1);
-                    map[i][j] = 0;
+                    dfs(map, count + 1);
+                    map[i][j] = 0; // 3개의 벽을 세우고 bfs까지 마무리하면 다음 벽들을 세우기 위해 벽을 초기화
                 }
             }
         }
     }
 
-    private static void bfs() {
-        Queue<Node> queue = new LinkedList<>();
-        int count = 0;
+    /* 바이러스가 맵에 퍼진 상황을 만들기 위한 bfs 메소드 */
+    public static void bfs(int[][] map) {
+        Queue<Node> q = new LinkedList<>();
 
-        /* 우선 연구소에서 바이러스가 위치한 좌표들을 큐에 삽입 */
-        for(int i = 0; i < N; i++) {
-            for(int j = 0; j < M; j++) {
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
                 if(map[i][j] == 2) {
-                    queue.add(new Node(i, j));
+                    q.add(new Node(i, j));
                 }
             }
         }
 
         /* 맵을 변경하지 않기 위해 복사 */
-        int[][] demoMap = new int[N][M];
+        int[][] demoMap = new int[n][m];
 
-        for(int i = 0; i < N; i++) {
+        for(int i = 0; i < n; i++) {
             demoMap[i] = map[i].clone();
         }
 
-        while(!queue.isEmpty()) {
-            Node node = queue.poll();
+        /* 맵에 바이러스가 퍼진 부분을 표시 */
+        while(!q.isEmpty()) {
+            Node tmp = q.poll();
 
             for(int i = 0; i < 4; i++) {
-                int x = node.x + dx[i];
-                int y = node.y + dy[i];
+                int nx = tmp.x + dx[i];
+                int ny = tmp.y + dy[i];
 
-                if(x >= 0 && y >= 0 && x < N && y < M) {
-                    if(demoMap[x][y] == 0) {
-                        demoMap[x][y] = 2;
-                        queue.add(new Node(x, y));
+                if(nx >= 0 && ny >= 0 && nx < n && ny < m) {
+                    if(demoMap[nx][ny] == 0) {
+                        q.add(new Node(nx, ny));
+                        demoMap[nx][ny] = 2;
                     }
                 }
             }
         }
 
-        for(int i = 0; i < N; i++) {
-            for(int j = 0; j < M; j++) {
+        /* 안전구역의 개수 카운트 */
+        int count = 0;
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
                 if(demoMap[i][j] == 0) {
                     count++;
                 }
             }
         }
 
-        safeAreaCount = safeAreaCount < count ? count : safeAreaCount;
+        /* 기존의 안전구역의 개수와 새로 계산한 안전구역의 개수 중 더 많은 개수를 유지 */
+        safeArea = Math.max(count, safeArea);
     }
 }
