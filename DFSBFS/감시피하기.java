0bb33d19afa29666add39c6f5cf9a6bfe8f23269
musichead99 <musichead99@naver.com>
2023-02-22 07:20:59 +0900

    DFS/BFS 기출문제 6번 감시 피하기

--- DFSBFS/감시피하기.java
+++ DFSBFS/감시피하기.java
@@ -7,83 +7,86 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.StringTokenizer;
 
-public class 감시피하기 {
+class Teacher {
+    int x;
+    int y;
 
-    private static class Teacher {
-        public int x;
-        public int y;
-
-        Teacher(int x, int y) {
-            this.x = x;
-            this.y = y;
-        }
+    public Teacher(int x, int y) {
+        this.x = x;
+        this.y = y;
     }
+}
 
+public class 감시피하기 {
     static char[][] map;
     static int n;
-    static String result = "NO";
-    static int[] dx = {1, 0, 0, -1};
-    static int[] dy = {0, 1, -1, 0};
-    static List<Teacher> teachers = new ArrayList<>();
-
-    public static void main(String[] args) throws NumberFormatException, IOException {
+    static String answer = "NO";
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
-
         n = Integer.parseInt(br.readLine());
-        map = new char[n][n];
+        map = new char[n + 1][n + 1];
 
-        for(int i = 0; i < n; i++) {
+        for(int i = 1; i <= n; i++) {
             st = new StringTokenizer(br.readLine());
-            for(int j = 0; j < n; j++) {
+            for(int j = 1; j <= n; j++) {
                 map[i][j] = st.nextToken().charAt(0);
-                if(map[i][j] == 'T') {
-                    teachers.add(new Teacher(i, j));
-                }
             }
         }
 
         dfs(0);
 
-        System.out.println(result);
+        System.out.println(answer);
+
     }
 
-    private static void dfs(int walls) {
-        if(walls == 3) {
-            searching();
+    /* 벽을 3개 세우는 경우의 수를 dfs탐색 */
+    public static void dfs(int count) {
+        if(count == 3) {
+            search();
             return;
         }
 
-        /* 벽 설치 */
-        for(int i = 0; i < n; i++) {
-            for(int j = 0; j < n; j++) {
-                if(map[i][j] == 'X') {
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j <= n; j++) {
+                if(map[i][j] == 'X') { // 'X'일 경우에만 벽 설치
                     map[i][j] = 'O';
-                    dfs(walls + 1);
+                    dfs(count + 1);
                     map[i][j] = 'X';
                 }
             }
         }
     }
 
-    private static void searching() {
-        for(Teacher t : teachers) {
-            for(int i = 0; i < 4; i++) {
-                int tx = t.x;
-                int ty = t.y;
-                while(tx >= 0 && ty >= 0 && tx < n && ty < n) {
-                    if(map[tx][ty] == 'O') {
-                        break;
-                    }
+    /* 교사들이 학생들을 탐지하는 메소드 */
+    public static void search() {
 
-                    if(map[tx][ty] == 'S') {
-                        return;
-                    }
-                    tx += dx[i];
-                    ty += dy[i];
+        /* 현재 맵에 위치한 교사들의 좌표를 리스트에 저장 */
+        List<Teacher> list = new ArrayList<>();
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j <= n; j++) {
+                if(map[i][j] == 'T') list.add(new Teacher(i, j));
+            }
+        }
+
+        /* 각 교사별로 탐색할 수 있는 모든 방향으로 탐색 시작 */
+        for(Teacher teacher : list) {
+            for(int i = 0; i < 4; i++) {
+                int nx = teacher.x;
+                int ny = teacher.y;
+                while(nx > 0 && ny > 0 && nx <= n && ny <= n) {
+                    if(map[nx][ny] == 'O') break; // 벽을 발견했다면 다음 방향으로 탐색
+                    
+                    if(map[nx][ny] == 'S') return; // 학생을 발견했다면 탐색 종료
+
+                    nx += dx[i];
+                    ny += dy[i];
                 }
             }
         }
-        result = "YES";
+        answer = "YES"; // 탐색을 모두 마쳐도 학생을 발견하지 못했을 경우 "YES"
+        return;
     }
 }
