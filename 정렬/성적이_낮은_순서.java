b319bbd63b9d34879c0a63e8bd1858245b364e85
musichead99 <musichead99@naver.com>
2023-02-24 11:43:46 +0900

    단순 수정

--- 정렬/성적이_낮은_순서.java
+++ 정렬/성적이_낮은_순서.java
@@ -7,21 +7,23 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 
-class Student implements Comparable<Student> {
-    String name;
-    int score;
-
-    public Student(String name, int score) {
-        this.name = name;
-        this.score = score;
-    }
-
-    @Override
-    public int compareTo(Student o) {
-        return this.score - o.score;
-    }
-}
 public class 성적이_낮은_순서 {
+
+    static class Student implements Comparable<Student> {
+        String name;
+        int score;
+    
+        public Student(String name, int score) {
+            this.name = name;
+            this.score = score;
+        }
+    
+        @Override
+        public int compareTo(Student o) {
+            return this.score - o.score;
+        }
+    }
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
