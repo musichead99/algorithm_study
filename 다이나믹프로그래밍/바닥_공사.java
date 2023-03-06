40ae4383b0bae472b1ce216d7b47b23884d18c67
musichead99 <musichead99@naver.com>
2023-03-06 11:16:05 +0900

    다이나믹프로그래밍 실전문제 3번 바닥 공사

--- 다이나믹프로그래밍/바닥_공사.java
+++ 다이나믹프로그래밍/바닥_공사.java
@@ -5,22 +5,20 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class 바닥_공사 {
-    public static void main(String[] args) throws NumberFormatException, IOException {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int N = Integer.parseInt(br.readLine());
-        br.close();
+        int n = Integer.parseInt(br.readLine());
 
-        int[] dp = new int[N];
+        int[] dp = new int[1001];
 
-        dp[0] = 1;
-        dp[1] = 3;
+        dp[1] = 1;
+        dp[2] = 3;
 
-        for(int i = 2; i < N; i++) {
-            dp[i] = dp[i-1] + dp[i-2] * 2;
+        for(int i = 3; i <= n; i++) {
+            dp[i] = (dp[i - 1] + dp[i - 2] * 2) % 796796;
         }
 
-        int result = dp[N-1] % 796796;
-        System.out.println(result);
+        System.out.println(dp[n]);
     }
 }
