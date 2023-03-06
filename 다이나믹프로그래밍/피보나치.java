8197ec358a5cd71a1f259ee00f4ae4abb02bca2e
musichead99 <musichead99@naver.com>
2023-03-06 10:43:23 +0900

    다이나믹프로그래밍 연습문제 1번 피보나치

--- 다이나믹프로그래밍/피보나치.java
+++ 다이나믹프로그래밍/피보나치.java
@@ -12,11 +12,11 @@ public class 피보나치 {
 
         int[] dp = new int[N];
 
-        for(int i = 0; i < N; i++) {
-            if(i == 0 || i == 1) dp[i] = 1;
-            else {
-                dp[i] = dp[i-1] + dp[i-2];
-            }
+        dp[0] = 1;
+        dp[1] = 1;
+
+        for(int i = 2; i < N; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
         }
 
         System.out.println(dp[N-1]);
