e9d62217b9a9e7b4a6e71e2e48c085040f4e630a
musichead99 <musichead99@naver.com>
2023-03-06 11:16:23 +0900

    다이나믹프로그래밍 실전문제 4번 효율적인 화폐 구성

--- 다이나믹프로그래밍/효율적인_화폐_구성.java
+++ 다이나믹프로그래밍/효율적인_화폐_구성.java
@@ -12,27 +12,27 @@ public class 효율적인_화폐_구성 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
 
-        int[] coinTypes = new int[N];
-        for(int i = 0; i < N; i++) {
-            coinTypes[i] = Integer.parseInt(br.readLine());
+        int[] coins = new int[n];
+
+        for(int i = 0; i < n; i++) {
+            coins[i] = Integer.parseInt(br.readLine());
         }
 
-        int[] dp = new int[M + 1];
-        Arrays.fill(dp, MAX_VALUE);
-
-        /* 다이나믹프로그래밍 시작 */
+        int[] dp = new int[101];
+        Arrays.fill(dp, 10001);
         dp[0] = 0;
-        for(int coin : coinTypes) {
-            for(int i = coin; i < M + 1; i++) {
-                if(dp[i - coin] != MAX_VALUE) {
-                    dp[i] = Math.min(dp[i - coin] + 1, i / coin);
+
+        for(int coin : coins) {
+            for(int i = coin; i <= m; i++) {
+                if(dp[i - coin] != 10001) {
+                    dp[i] = Math.min(dp[i], dp[i - coin] + 1);
                 }
             }
         }
-            if(dp[M] != 10001) System.out.println(dp[M]);
-            else System.out.println(-1);
+
+        System.out.println(dp[m]);
     }
 }
