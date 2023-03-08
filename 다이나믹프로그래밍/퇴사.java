110716ab31e9fb53d7cb6610311f48d3912f7df0
musichead99 <musichead99@naver.com>
2023-03-08 10:46:41 +0900

    주석 달기

--- 다이나믹프로그래밍/퇴사.java
+++ 다이나믹프로그래밍/퇴사.java
@@ -26,7 +26,7 @@ public class 퇴사 {
 
         int[] dp = new int[n + 1];
         Node[] counsels = new Node[n + 1];
-        int maxValue = 0;
+        int maxValue = 0; // 얻을 수 있는 이익의 최댓값을 항상 저장한다.
 
         for(int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
@@ -35,12 +35,15 @@ public class 퇴사 {
             counsels[i] = new Node(t, p);
         }
 
+        /* 리스트를 거꾸로 탐색하며 다이나믹프로그래밍 시작 */
         for(int i = n - 1; i >= 0; i--) {
             Node tmp = counsels[i];
 
+            /* 만약 해당 상담 일정이 퇴사 전에 수행 가능하다면 */
             if(tmp.t + i < n + 1) {
+                /* 해당 상담 일정을 잡았을 때 얻을 수 있는 이익과 현재 저장된 최대 이익값을 비교 */
                 dp[i] = Math.max(dp[tmp.t + i] + tmp.p, maxValue);
-                maxValue = dp[i];
+                maxValue = dp[i]; // 이익의 최댓값
             } else {
                 dp[i] = maxValue;
             }
