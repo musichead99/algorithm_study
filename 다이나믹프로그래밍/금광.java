7042b044650e4353a9858d3e35ccde90276a12af
musichead99 <musichead99@naver.com>
2023-03-08 10:01:19 +0900

    다이나믹프로그래밍 깇출문제 1번 금광

--- 다이나믹프로그래밍/금광.java
+++ 다이나믹프로그래밍/금광.java
@@ -8,39 +8,35 @@ import java.util.StringTokenizer;
 public class 금광 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
         StringTokenizer st;
 
-        int T = Integer.parseInt(br.readLine());
-        int[] answers = new int[T];
+        int t = Integer.parseInt(br.readLine());
 
-        for(int answer : answers) {
+        for(int i = 0; i < t; i++) {
             st = new StringTokenizer(br.readLine());
+
             int n = Integer.parseInt(st.nextToken());
             int m = Integer.parseInt(st.nextToken());
+
+            /* 금광에 금 정보 입력하기 */
             int[][] map = new int[n + 2][m + 1];
-            int[][] dp = new int[n + 2][m + 1];
-
             st = new StringTokenizer(br.readLine());
-            for(int i = 1; i <= n; i++) {
-                for(int j = 1; j <= m; j++) {
-                    map[i][j] = Integer.parseInt(st.nextToken());
+            for(int j = 1; j < n + 1; j++) {
+                for(int k = 1; k < m + 1; k++) {
+                    map[j][k] = Integer.parseInt(st.nextToken());
                 }
             }
 
-            /* 왼쪽 위, 왼쪽 옆, 왼쪽 아래의 3가지 방향에서 오는 경우를 고려한다. */
-            for(int j = 1; j <= m; j++) {
-                for(int i = 1; i <= n; i++) {
-                    dp[i][j] = Math.max(Math.max(dp[i - 1][j - 1], dp[i][j - 1]), dp[i + 1][j - 1]) + map[i][j];
+            /* 현 시점에서 얻을 수 있는 금의 최대 크기 = 해당 지점의 왼쪽 위, 왼쪽, 왼쪽 아래 중 얻을 수 있는 가장 큰 금의 크기 + 현재 금의 크기 */
+            int[][] dp = new int[n + 2][m + 1];
+            for(int k = 1; k < m + 1; k++) {
+                for(int j = 1; j < n + 1; j++) {
+                    dp[j][k] = Math.max(Math.max(dp[j - 1][k - 1], dp[j][k - 1]), dp[j + 1][k - 1]) + map[j][k];
                 }
             }
 
-            answer = Math.max(Math.max(dp[n][m], dp[n - 1][m]), dp[n + 1][m]);
-            sb.append(answer + "\n");
+            int result = Math.max(Math.max(dp[n][m], dp[n - 1][m]), dp[n - 2][m]);
+            System.out.println(result);
         }
-
-        br.close();
-
-        System.out.print(sb.toString());
     }
 }
